72fcdca889f2c771398630fc4481ba767306dcc9
imtiyaz597 <imtiyazagasimani@gmail.com>
2025-05-05 11:32:05 +0530

    save

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ app.use('/data', express.static(path.join(__dirname, 'data')));
 app.use(express.json({ limit: '10mb' }));
 app.use(bodyParser.json({ limit: '10mb' }));
 
-const allowedOrigins = process.env.CORS_ORIGINS?.split(',') || [];
+const allowedOrigins = [
+  'https://full-stack-mock-website.vercel.app',
+  'https://edzest-website.vercel.app'
+];
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -28,9 +31,6 @@ app.use(cors({
 }));
 
 
-app.use(cors({
-  origin: 'https://full-stack-mock-website.vercel.app'
-}));
 
 // Connect to MongoDB using environment variable
 mongoose
